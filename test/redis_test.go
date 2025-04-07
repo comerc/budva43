2f46c1086cbe65036b8b40cb5771a92e91ff89a0
comerc <andrew.kachanov@gmail.com>
2025-04-07 05:41:26 +0000

    Move to /test

---
+++ test/redis_test.go
@@ -0,0 +1,31 @@
+package redis_test
+
+import (
+	"context"
+	"log"
+	"path/filepath"
+	"testing"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/redis"
+)
+
+func TestRedis(m *testing.M) {
+	ctx := context.Background()
+
+	redisContainer, err := redis.Run(ctx,
+		"redis:7",
+		redis.WithSnapshotting(10, 1),
+		redis.WithLogLevel(redis.LogLevelVerbose),
+		redis.WithConfigFile(filepath.Join("testdata", "redis7.conf")),
+	)
+	defer func() {
+		if err := testcontainers.TerminateContainer(redisContainer); err != nil {
+			log.Printf("failed to terminate container: %s", err)
+		}
+	}()
+	if err != nil {
+		log.Printf("failed to start container: %s", err)
+		return
+	}
+}
