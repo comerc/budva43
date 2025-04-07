302bcbc5c39700bfef7ed0e3246d857a45dc5926
comerc <andrew.kachanov@gmail.com>
2025-04-07 05:13:47 +0000

    Add test conventions

--- main.go
+++ main.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 )
 
+// Когда хочется узнать место в коде, которое пишет сообщение в лог, можно использовать:
+// slog.HandlerOptions{
+// 	AddSource: true,
+// 	 // ...
+//   }
+
 func main() {
 	fmt.Println("Hello, World!")
 }
