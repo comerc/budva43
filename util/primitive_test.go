9c9af5110a41c6b745d7d913bc2310d4457daad4
comerc <andrew.kachanov@gmail.com>
2025-05-03 09:30:23 +0300

    Typo

--- util/primitive_test.go
+++ util/primitive_test.go
@@ -61,7 +61,7 @@ func TestCopy(t *testing.T) {
 	obj2.I = 2
 	obj2.A[0] = "c"
 	obj2.M["a"] = "c"
-	assert.Equal(t, obj1.I, int64(1), "obj1 не изменился")
-	assert.Equal(t, obj1.A, []string{"a", "b"}, "obj1 не изменился")
-	assert.Equal(t, obj1.M, map[string]string{"a": "b"}, "obj1 не изменился")
+	assert.Equal(t, obj1.I, int64(1), "obj1.I не изменился")
+	assert.Equal(t, obj1.A, []string{"a", "b"}, "obj1.A не изменился")
+	assert.Equal(t, obj1.M, map[string]string{"a": "b"}, "obj1.M не изменился")
 }
