a16f09e49633a5f58000a3cf62881fa1f2c5a230
comerc <andrew.kachanov@gmail.com>
2025-05-17 12:31:16 +0300

    Add addAutoAnswer() to transformService.AddSources()

--- service/transform/service.go
+++ service/transform/service.go
@@ -17,19 +17,29 @@ type telegramRepo interface {
 	GetClient() *client.Client
 }
 
+// messageService определяет методы для работы с сообщениями
+type messageService interface {
+	GetReplyMarkupData(message *client.Message) ([]byte, bool)
+}
+
 // Service предоставляет методы для преобразования и замены текста
 type Service struct {
 	log *slog.Logger
 	//
-	telegramRepo telegramRepo
+	telegramRepo   telegramRepo
+	messageService messageService
 }
 
 // New создает новый экземпляр сервиса для работы с текстовыми трансформациями
-func New(telegramRepo telegramRepo) *Service {
+func New(
+	telegramRepo telegramRepo,
+	messageService messageService,
+) *Service {
 	return &Service{
 		log: slog.With("module", "service.transform"),
 		//
-		telegramRepo: telegramRepo,
+		telegramRepo:   telegramRepo,
+		messageService: messageService,
 	}
 }
 
@@ -45,10 +55,10 @@ func (s *Service) ReplaceFragments(formattedText *client.FormattedText, dstChatI
 	return nil
 }
 
-// addSource добавляет источник к тексту
-func (s *Service) addSource(formattedText *client.FormattedText, title string) error {
-	parsedTitle, err := s.telegramRepo.GetClient().ParseTextEntities(&client.ParseTextEntitiesRequest{
-		Text: title,
+// addText добавляет новый текст в конец форматированного текста
+func (s *Service) addText(formattedText *client.FormattedText, text string) error {
+	parsedText, err := s.telegramRepo.GetClient().ParseTextEntities(&client.ParseTextEntitiesRequest{
+		Text: text,
 		ParseMode: &client.TextParseModeMarkdown{
 			Version: 2,
 		},
@@ -61,43 +71,67 @@ func (s *Service) addSource(formattedText *client.FormattedText, title string) e
 		formattedText.Text += "\n\n"
 		offset = offset + 2
 	}
-	for _, entity := range parsedTitle.Entities {
+	for _, entity := range parsedText.Entities {
 		entity.Offset += offset
 	}
-	formattedText.Text += parsedTitle.Text
-	formattedText.Entities = append(formattedText.Entities, parsedTitle.Entities...)
+	formattedText.Text += parsedText.Text
+	formattedText.Entities = append(formattedText.Entities, parsedText.Entities...)
 	return nil
 }
 
 // AddSources добавляет подпись и ссылку на источник к тексту
-func (s *Service) AddSources(formattedText *client.FormattedText, message *client.Message, dstChatId int64) error {
-	source, ok := config.Engine.Sources[message.ChatId]
+func (s *Service) AddSources(formattedText *client.FormattedText, src *client.Message, dstChatId int64) error {
+	source, ok := config.Engine.Sources[src.ChatId]
 	if !ok {
 		return nil
 	}
+	if source.AutoAnswer {
+		var err error
+		data, ok := s.messageService.GetReplyMarkupData(src)
+		if !ok {
+			return nil
+		}
+		answer, err := s.telegramRepo.GetClient().GetCallbackQueryAnswer(
+			&client.GetCallbackQueryAnswerRequest{
+				ChatId:    src.ChatId,
+				MessageId: src.Id,
+				Payload:   &client.CallbackQueryPayloadData{Data: data},
+			},
+		)
+		if err != nil {
+			return fmt.Errorf("GetCallbackQueryAnswer: %w", err)
+		}
+		err = s.addText(formattedText, escapeMarkdown(answer.Text))
+		if err != nil {
+			return fmt.Errorf("addText for answer: %w", err)
+		}
+	}
 	if slices.Contains(source.Sign.For, dstChatId) {
-		title := source.Sign.Title
-		return s.addSource(formattedText, title)
+		var err error
+		text := source.Sign.Title
+		err = s.addText(formattedText, text)
+		if err != nil {
+			return fmt.Errorf("addText for sign: %w", err)
+		}
 	}
 	if slices.Contains(source.Link.For, dstChatId) {
+		var err error
 		messageLink, err := s.telegramRepo.GetClient().GetMessageLink(&client.GetMessageLinkRequest{
-			ChatId:    message.ChatId,
-			MessageId: message.Id,
-			ForAlbum:  message.MediaAlbumId != 0,
+			ChatId:    src.ChatId,
+			MessageId: src.Id,
+			ForAlbum:  src.MediaAlbumId != 0,
 			// ForComment: false, // удалено в новой версии go-tdlib
 		})
 		if err != nil {
 			return fmt.Errorf("GetMessageLink: %w", err)
 		}
-		title := fmt.Sprintf("[%s%s](%s)", "\U0001f517", source.Link.Title, messageLink.Link)
-		return s.addSource(formattedText, title)
+		text := fmt.Sprintf("[%s%s](%s)", "\U0001f517", source.Link.Title, messageLink.Link)
+		err = s.addText(formattedText, text)
+		if err != nil {
+			return fmt.Errorf("addText for link: %w", err)
+		}
 	}
 	return nil
-}
-
-// AddAutoAnswer добавляет ответ на сообщение
-func (s *Service) AddAutoAnswer(formattedText *client.FormattedText, src *client.Message) {
-	// TODO: выполнить корректный перенос из budva32
 }
 
 // escapeMarkdown экранирует специальные символы Markdown в тексте
