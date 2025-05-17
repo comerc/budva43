c607bf5013d66da4d725b8f729e41e88bdf0dde7
comerc <andrew.kachanov@gmail.com>
2025-05-17 10:06:02 +0300

    Add transformService.addSourceSign()

--- service/transform/service.go
+++ service/transform/service.go
@@ -2,120 +2,34 @@ package transform
 
 import (
 	"log/slog"
-	"regexp"
 	"slices"
 	"strings"
 
 	"github.com/zelenin/go-tdlib/client"
 
 	"github.com/comerc/budva43/config"
+	"github.com/comerc/budva43/util"
 )
+
+// telegramRepo определяет методы для работы с Telegram API
+type telegramRepo interface {
+	GetClient() *client.Client
+}
 
 // Service предоставляет методы для преобразования и замены текста
 type Service struct {
 	log *slog.Logger
 	//
-	// Здесь могут быть зависимости, например, репозитории
+	telegramRepo telegramRepo
 }
 
 // New создает новый экземпляр сервиса для работы с текстовыми трансформациями
-func New() *Service {
+func New(telegramRepo telegramRepo) *Service {
 	return &Service{
 		log: slog.With("module", "service.transform"),
 		//
+		telegramRepo: telegramRepo,
 	}
-}
-
-// // GetReplacement возвращает текст для замены или пустую строку, если замена не найдена
-// func (s *Service) GetReplacement(settings *entity.ReplaceFragment, text string) string {
-// 	if settings.Replacements == nil {
-// 		return ""
-// 	}
-// 	replacement, ok := settings.Replacements[text]
-// 	if !ok {
-// 		return ""
-// 	}
-// 	return replacement
-// }
-
-// // ReplaceText заменяет все фрагменты текста согласно настройкам
-// func (s *Service) ReplaceText(settings *entity.ReplaceFragment, text string) string {
-// 	if settings.Replacements == nil {
-// 		return text
-// 	}
-
-// 	result := text
-// 	for from, to := range settings.Replacements {
-// 		// Здесь может быть реализован более сложный алгоритм замены,
-// 		// но для простоты используем стандартную замену строк
-// 		if from != "" {
-// 			result = s.replaceAll(result, from, to)
-// 		}
-// 	}
-
-// 	return result
-// }
-
-// replaceAll заменяет все вхождения подстроки в строке
-// Используется вместо strings.ReplaceAll для возможности
-// реализации более сложной логики замены в будущем
-// func (s *Service) replaceAll(text, from, to string) string {
-// 	return strings.ReplaceAll(text, from, to)
-// }
-
-// ReplaceLinks заменяет ссылки в тексте согласно настройкам
-func (s *Service) ReplaceLinks(text string, linkReplacements map[string]string) string {
-	if len(linkReplacements) == 0 {
-		return text
-	}
-
-	result := text
-	for from, to := range linkReplacements {
-		result = strings.ReplaceAll(result, from, to)
-	}
-
-	return result
-}
-
-// RemoveUnwantedContent удаляет нежелательный контент из текста
-func (s *Service) RemoveUnwantedContent(text string, patterns []string) string {
-	if len(patterns) == 0 {
-		return text
-	}
-
-	result := text
-	for _, pattern := range patterns {
-		re, err := regexp.Compile(pattern)
-		if err != nil {
-			continue
-		}
-		result = re.ReplaceAllString(result, "")
-	}
-
-	return result
-}
-
-// FormatText форматирует текст согласно заданным правилам
-func (s *Service) FormatText(text string, rules map[string]string) string {
-	if len(rules) == 0 {
-		return text
-	}
-
-	result := text
-
-	// Пример правила: "bold" -> "**{text}**"
-	if boldRule, ok := rules["bold"]; ok {
-		boldPattern := regexp.MustCompile(`\*\*(.*?)\*\*`)
-		result = boldPattern.ReplaceAllString(result, boldRule)
-	}
-
-	// Пример правила: "italic" -> "_{text}_"
-	if italicRule, ok := rules["italic"]; ok {
-		italicPattern := regexp.MustCompile(`_(.*?)_`)
-		result = italicPattern.ReplaceAllString(result, italicRule)
-	}
-
-	return result
 }
 
 // ReplaceMyselfLinks заменяет ссылки на текущего бота в тексте
@@ -132,7 +46,27 @@ func (s *Service) ReplaceFragments(formattedText *client.FormattedText, dstChatI
 
 // addSourceSign добавляет подпись источника к тексту
 func (s *Service) addSourceSign(formattedText *client.FormattedText, title string) error {
-	// TODO: выполнить корректный перенос из budva32
+	sourceSign, err := s.telegramRepo.GetClient().ParseTextEntities(&client.ParseTextEntitiesRequest{
+		Text: title,
+		ParseMode: &client.TextParseModeMarkdown{
+			Version: 2,
+		},
+	})
+	if err != nil {
+		s.log.Error("ParseTextEntities", "err", err)
+	} else {
+		offset := int32(util.RuneCountForUTF16(formattedText.Text))
+		if offset > 0 {
+			formattedText.Text += "\n\n"
+			offset = offset + 2
+		}
+		for _, entity := range sourceSign.Entities {
+			entity.Offset += offset
+		}
+		formattedText.Text += sourceSign.Text
+		formattedText.Entities = append(formattedText.Entities, sourceSign.Entities...)
+	}
+	s.log.Info("addSourceSign", "formattedText", formattedText)
 	return nil
 }
 
