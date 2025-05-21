6df37225a2fced7e03ae0ea1727f2e8f443876aa
comerc <andrew.kachanov@gmail.com>
2025-05-21 19:41:50 +0300

    Rename to h

--- handler/update_delete_messages/handler.go
+++ handler/update_delete_messages/handler.go
@@ -50,7 +50,7 @@ func New(
 	}
 }
 
-func (s *Handler) Run(update *client.UpdateDeleteMessages) {
+func (h *Handler) Run(update *client.UpdateDeleteMessages) {
 	if !update.IsPermanent {
 		return
 	}
@@ -59,43 +59,43 @@ func (s *Handler) Run(update *client.UpdateDeleteMessages) {
 		return
 	}
 
-	s.process(update.ChatId, update.MessageIds)
+	h.process(update.ChatId, update.MessageIds)
 }
 
-func (s *Handler) process(chatId int64, messageIds []int64) {
+func (h *Handler) process(chatId int64, messageIds []int64) {
 	const maxRetries = 3
 	retryCount := 0
 
 	var fn func()
 	fn = func() {
-		data, err := s.collectData(chatId, messageIds)
+		data, err := h.collectData(chatId, messageIds)
 		if err != nil {
 			retryCount++
 			if retryCount >= maxRetries {
-				s.log.Error("max retries reached for message deletion",
+				h.log.Error("max retries reached for message deletion",
 					"chatId", chatId,
 					"messageIds", messageIds,
 				)
 				return
 			}
-			s.log.Info("retrying message deletion",
+			h.log.Info("retrying message deletion",
 				"retryCount", retryCount,
 				"chatId", chatId,
 				"messageIds", messageIds,
 			)
-			s.queueRepo.Add(fn) // переставляем в конец очереди
+			h.queueRepo.Add(fn) // переставляем в конец очереди
 			return
 		}
 
-		result := s.deleteMessages(chatId, messageIds, data)
-		s.log.Info("deleteMessages",
+		result := h.deleteMessages(chatId, messageIds, data)
+		h.log.Info("deleteMessages",
 			"chatId", chatId,
 			"messageIds", messageIds,
 			"result", result,
 		)
 	}
 
-	s.queueRepo.Add(fn)
+	h.queueRepo.Add(fn)
 }
 
 type data struct {
@@ -103,7 +103,7 @@ type data struct {
 	newMessageIds    map[string]int64    // tmpChatMessageId -> newMessageId
 }
 
-func (s *Handler) collectData(chatId int64, messageIds []int64) (*data, error) {
+func (h *Handler) collectData(chatId int64, messageIds []int64) (*data, error) {
 	data := &data{
 		copiedMessageIds: make(map[string][]string),
 		newMessageIds:    make(map[string]int64),
@@ -111,7 +111,7 @@ func (s *Handler) collectData(chatId int64, messageIds []int64) (*data, error) {
 
 	for _, messageId := range messageIds {
 		fromChatMessageId := fmt.Sprintf("%d:%d", chatId, messageId)
-		toChatMessageIds, _ := s.storageService.GetCopiedMessageIds(fromChatMessageId)
+		toChatMessageIds, _ := h.storageService.GetCopiedMessageIds(fromChatMessageId)
 		data.copiedMessageIds[fromChatMessageId] = toChatMessageIds
 
 		for _, toChatMessageId := range toChatMessageIds {
@@ -120,7 +120,7 @@ func (s *Handler) collectData(chatId int64, messageIds []int64) (*data, error) {
 			dstChatId := util.ConvertToInt[int64](a[1])
 			tmpMessageId := util.ConvertToInt[int64](a[2])
 
-			newMessageId, _ := s.storageService.GetNewMessageId(dstChatId, tmpMessageId)
+			newMessageId, _ := h.storageService.GetNewMessageId(dstChatId, tmpMessageId)
 			if newMessageId == 0 {
 				return nil, fmt.Errorf("newMessageId не найден")
 			}
@@ -133,7 +133,7 @@ func (s *Handler) collectData(chatId int64, messageIds []int64) (*data, error) {
 	return data, nil
 }
 
-func (s *Handler) deleteMessages(chatId int64, messageIds []int64, data *data) []string {
+func (h *Handler) deleteMessages(chatId int64, messageIds []int64, data *data) []string {
 	result := make([]string, 0)
 
 	for _, messageId := range messageIds {
@@ -148,7 +148,7 @@ func (s *Handler) deleteMessages(chatId int64, messageIds []int64, data *data) [
 
 			forwardRule, ok := config.Engine.ForwardRules[forwardRuleId]
 			if !ok {
-				s.log.Error("forwardRule not found",
+				h.log.Error("forwardRule not found",
 					"forwardRuleId", forwardRuleId,
 					"fromChatMessageId", fromChatMessageId,
 					"toChatMessageId", toChatMessageId,
@@ -159,22 +159,22 @@ func (s *Handler) deleteMessages(chatId int64, messageIds []int64, data *data) [
 				continue
 			}
 
-			_ = s.storageService.DeleteAnswerMessageId(dstChatId, tmpMessageId)
+			_ = h.storageService.DeleteAnswerMessageId(dstChatId, tmpMessageId)
 
 			tmpChatMessageId := fmt.Sprintf("%d:%d", dstChatId, tmpMessageId)
 			newMessageId := data.newMessageIds[tmpChatMessageId]
 
 			// TODO: может лучше удалять индексы после удаления сообщения?
-			_ = s.storageService.DeleteTmpMessageId(dstChatId, newMessageId)
-			_ = s.storageService.DeleteNewMessageId(dstChatId, tmpMessageId)
+			_ = h.storageService.DeleteTmpMessageId(dstChatId, newMessageId)
+			_ = h.storageService.DeleteNewMessageId(dstChatId, tmpMessageId)
 
-			_, err := s.telegramRepo.GetClient().DeleteMessages(&client.DeleteMessagesRequest{
+			_, err := h.telegramRepo.GetClient().DeleteMessages(&client.DeleteMessagesRequest{
 				ChatId:     dstChatId,
 				MessageIds: []int64{newMessageId},
 				Revoke:     true,
 			})
 			if err != nil {
-				s.log.Error("DeleteMessages", "err", err)
+				h.log.Error("DeleteMessages", "err", err)
 				continue
 			}
 
@@ -183,7 +183,7 @@ func (s *Handler) deleteMessages(chatId int64, messageIds []int64, data *data) [
 		}
 
 		if len(toChatMessageIds) > 0 {
-			_ = s.storageService.DeleteCopiedMessageIds(fromChatMessageId)
+			_ = h.storageService.DeleteCopiedMessageIds(fromChatMessageId)
 		}
 	}
 
