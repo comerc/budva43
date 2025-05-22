5ddc33065b3c5e098ccca7ff7d3a079bc96003ce
comerc <andrew.kachanov@gmail.com>
2025-05-22 16:58:55 +0300

    Refactor updateMessageEditedHandler

--- handler/update_message_edited/handler.go
+++ handler/update_message_edited/handler.go
@@ -1,9 +1,7 @@
 package update_message_edited
 
 import (
-	"context"
 	"fmt"
-	"log"
 	"log/slog"
 	"strings"
 
@@ -48,7 +46,6 @@ type forwarderService interface {
 
 type Handler struct {
 	log *slog.Logger
-	ctx context.Context
 	//
 	telegramRepo       telegramRepo
 	queueRepo          queueRepo
@@ -88,181 +85,226 @@ func (h *Handler) Run(update *client.UpdateMessageEdited) {
 		return
 	}
 	messageId := update.MessageId
-	repeat := 0
+
+	const maxRetries = 3
+	retryCount := 0
+
 	var fn func()
 	fn = func() {
-		var (
-			result []string
-			src    *client.Message
-		)
-		fromChatMessageId := fmt.Sprintf("%d:%d", chatId, messageId)
-		toChatMessageIds, _ := h.storageService.GetCopiedMessageIds(fromChatMessageId)
-		defer func() {
-			args := []any{
-				"fromChatMessageId", fromChatMessageId,
-				"toChatMessageIds", toChatMessageIds,
-			}
-			if src != nil {
-				args = append(args, "contentType", src.Content.MessageContentType())
-			}
-			if len(result) > 0 {
-				args = append(args, "result", result)
-			}
-			h.log.Info("UpdateMessageEdited", args...)
-		}()
-		if len(toChatMessageIds) == 0 {
-			return
-		}
-		var newMessageIds = make(map[string]int64)
-		isUpdateMessageSendSucceeded := true
-		for _, toChatMessageId := range toChatMessageIds {
-			a := strings.Split(toChatMessageId, ":")
-			// forwardRuleId := a[0]
-			dstChatId := util.ConvertToInt[int64](a[1])
-			tmpMessageId := util.ConvertToInt[int64](a[2])
-			newMessageId, _ := h.storageService.GetNewMessageId(dstChatId, tmpMessageId)
-			if newMessageId == 0 {
-				isUpdateMessageSendSucceeded = false
-				break
-			}
-			tmpChatMessageId := fmt.Sprintf("%d:%d", dstChatId, tmpMessageId)
-			newMessageIds[tmpChatMessageId] = newMessageId
-		}
-		if !isUpdateMessageSendSucceeded {
-			repeat++
-			if repeat < 3 {
-				log.Print("isUpdateMessageSendSucceeded > repeat: ", repeat)
-				h.queueRepo.Add(fn)
-			} else {
-				log.Print("isUpdateMessageSendSucceeded > repeat limit !!!")
-			}
-			return
-		}
-		src, err := h.telegramRepo.GetClient().GetMessage(&client.GetMessageRequest{
-			ChatId:    chatId,
-			MessageId: messageId,
-		})
+		data, err := h.collectData(chatId, messageId)
 		if err != nil {
-			h.log.Error("GetMessage", "err", err)
+			h.log.Error("collectData", "err", err)
 			return
+		}
+		if data.needRepeat {
+			retryCount++
+			if retryCount >= maxRetries {
+				h.log.Error("max retries reached for message edit",
+					"chatId", chatId,
+					"messageId", messageId,
+				)
+				return
+			}
+			h.log.Info("retrying message edit",
+				"retryCount", retryCount,
+				"chatId", chatId,
+				"messageId", messageId,
+			)
+			h.queueRepo.Add(fn)
+			return
+		}
+
+		result, _ := h.editMessages(chatId, messageId, data)
+		h.log.Info("editMessages",
+			"chatId", chatId,
+			"messageId", messageId,
+			"result", result,
+		)
+	}
+
+	h.queueRepo.Add(fn)
+}
+
+type data struct {
+	needRepeat       bool
+	copiedMessageIds []string         // []toChatMessageId
+	newMessageIds    map[string]int64 // tmpChatMessageId -> newMessageId
+}
+
+// collectData собирает данные для редактирования сообщений
+func (h *Handler) collectData(chatId, messageId int64) (*data, error) {
+	result := &data{}
+	// errs := []error{} // TODO: собирать все ошибки (для тестов)
+
+	fromChatMessageId := fmt.Sprintf("%d:%d", chatId, messageId)
+	toChatMessageIds, _ := h.storageService.GetCopiedMessageIds(fromChatMessageId)
+	result.copiedMessageIds = toChatMessageIds
+
+	result.newMessageIds = make(map[string]int64)
+
+	for _, toChatMessageId := range toChatMessageIds {
+		a := strings.Split(toChatMessageId, ":")
+		// forwardRuleId := a[0]
+		dstChatId := util.ConvertToInt[int64](a[1])
+		tmpMessageId := util.ConvertToInt[int64](a[2])
+
+		newMessageId, err := h.storageService.GetNewMessageId(dstChatId, tmpMessageId)
+		if err != nil {
+			result = &data{needRepeat: true}
+			return result, nil
+		}
+
+		tmpChatMessageId := fmt.Sprintf("%d:%d", dstChatId, tmpMessageId)
+		result.newMessageIds[tmpChatMessageId] = newMessageId
+	}
+
+	return result, nil
+}
+
+// editMessages редактирует сообщения
+func (h *Handler) editMessages(chatId, messageId int64, data *data) ([]string, error) {
+	result := []string{}
+	// errs := []error{} // TODO: собирать все ошибки (для тестов)
+
+	var err error
+	fromChatMessageId := fmt.Sprintf("%d:%d", chatId, messageId)
+	toChatMessageIds := data.copiedMessageIds
+
+	src, err := h.telegramRepo.GetClient().GetMessage(&client.GetMessageRequest{
+		ChatId:    chatId,
+		MessageId: messageId,
+	})
+	if err != nil {
+		return nil, err
+	}
+	// TODO: isAnswer
+	_, hasReplyMarkupData := h.messageService.GetReplyMarkupData(src)
+	srcFormattedText := h.messageService.GetFormattedText(src)
+	h.log.Info("editMessages",
+		"chatId", src.ChatId,
+		"messageId", src.Id,
+		"hasText", srcFormattedText.Text != "",
+		"mediaAlbumId", src.MediaAlbumId,
+	)
+
+	checkFns := make(map[int64]func())
+
+	for _, toChatMessageId := range toChatMessageIds {
+		a := strings.Split(toChatMessageId, ":")
+		forwardRuleId := a[0]
+		dstChatId := util.ConvertToInt[int64](a[1])
+		tmpMessageId := util.ConvertToInt[int64](a[2])
+
+		forwardRule, ok := config.Engine.ForwardRules[forwardRuleId]
+		if !ok {
+			h.log.Error("forwardRule not found",
+				"forwardRuleId", forwardRuleId,
+				// "fromChatMessageId", fromChatMessageId,
+				"toChatMessageId", toChatMessageId,
+			)
+			continue
+		}
+
+		if forwardRule.CopyOnce {
+			continue
+		}
+
+		formattedText := util.Copy(srcFormattedText)
+		if (forwardRule.SendCopy || src.CanBeSaved) &&
+			h.filtersModeService.Map(formattedText, forwardRule) == entity.FiltersCheck {
+			_, ok := checkFns[forwardRule.Check]
+			if !ok {
+				checkFns[forwardRule.Check] = func() {
+					const isSendCopy = false // обязательно надо форвардить, иначе не видно текущего сообщения
+					h.forwarderService.ForwardMessages([]*client.Message{src}, chatId, forwardRule.Check, isSendCopy, forwardRuleId)
+				}
+			}
+			continue
+		}
+
+		// TODO: почему не используется?
+		// hasFiltersCheck := false
+		// testChatId := dstChatId
+		// var src *client.Message
+		// for _, forwardRule := range config.Engine.ForwardRules {
+		// 	if src.ChatId == forwardRule.From && (forwardRule.SendCopy || src.CanBeSaved) {
+		// 		for _, dstChatId := range forwardRule.To {
+		// 			if testChatId == dstChatId {
+		// 				if h.filtersModeService.Map(formattedText, forwardRule) == entity.FiltersCheck {
+		// 					hasFiltersCheck = true
+		// 					_, ok := checkFns[forwardRule.Check]
+		// 					if !ok {
+		// 						checkFns[forwardRule.Check] = func() {
+		// 							const isSendCopy = false // обязательно надо форвардить, иначе не видно текущего сообщения
+		// 							h.forwarderService.ForwardMessages([]*client.Message{src}, chatId, forwardRule.Check, isSendCopy, forwardRule.Id)
+		// 						}
+		// 					}
+		// 				}
+		// 			}
+		// 		}
+		// 	}
+		// }
+		// if hasFiltersCheck {
+		// 	continue
+		// }
+
+		withSources := true
+		if err := h.transformService.Transform(formattedText, withSources, src, dstChatId); err != nil {
+			h.log.Error("Transform", "err", err)
+		}
+
+		tmpChatMessageId := fmt.Sprintf("%d:%d", dstChatId, tmpMessageId)
+		newMessageId := data.newMessageIds[tmpChatMessageId]
+		result = append(result, fmt.Sprintf("toChatMessageId: %s, newMessageId: %d", toChatMessageId, newMessageId))
+
+		switch src.Content.(type) {
+		case
+			*client.MessageText,
+			*client.MessageAnimation,
+			*client.MessageDocument,
+			*client.MessageAudio,
+			*client.MessageVideo,
+			*client.MessagePhoto:
+			content := h.messageService.GetInputMessageContent(src, formattedText)
+			dst, err := h.telegramRepo.GetClient().EditMessageText(&client.EditMessageTextRequest{
+				ChatId:              dstChatId,
+				MessageId:           newMessageId,
+				InputMessageContent: content,
+				// ReplyMarkup: func() client.ReplyMarkup {
+				// 	if src.Content.(type).MessageContentType() == client.TypeMessageText {
+				// 		return src.ReplyMarkup // это не надо, юзер-бот игнорит изменение
+				// 	}
+				// 	return nil
+				// }(),
+			})
+			if err != nil {
+				h.log.Error("EditMessageText", "err", err)
+			}
+			h.log.Info("EditMessageText", "dst", dst)
+		case *client.MessageVoiceNote:
+			dst, err := h.telegramRepo.GetClient().EditMessageCaption(&client.EditMessageCaptionRequest{
+				ChatId:    dstChatId,
+				MessageId: newMessageId,
+				Caption:   formattedText,
+			})
+			if err != nil {
+				h.log.Error("EditMessageCaption", "err", err)
+			}
+			h.log.Info("EditMessageCaption", "dst", dst)
+		default:
+			continue
 		}
 		// TODO: isAnswer
-		_, hasReplyMarkupData := h.messageService.GetReplyMarkupData(src)
-		srcFormattedText := h.messageService.GetFormattedText(src)
-		log.Printf("srcChatId: %d srcId: %d hasText: %t MediaAlbumId: %d", src.ChatId, src.Id, srcFormattedText.Text != "", src.MediaAlbumId)
-		checkFns := make(map[int64]func())
-		for _, toChatMessageId := range toChatMessageIds {
-			a := strings.Split(toChatMessageId, ":")
-			forwardRuleId := a[0]
-			dstChatId := util.ConvertToInt[int64](a[1])
-			tmpMessageId := util.ConvertToInt[int64](a[2])
-			formattedText := util.Copy(srcFormattedText)
-			forwardRule, ok := config.Engine.ForwardRules[forwardRuleId]
-			if !ok {
-				h.log.Error("forwardRule not found",
-					"forwardRuleId", forwardRuleId,
-					"fromChatMessageId", fromChatMessageId,
-					"toChatMessageId", toChatMessageId,
-				)
-				continue
-			}
-			if forwardRule.CopyOnce {
-				continue
-			}
-			if (forwardRule.SendCopy || src.CanBeSaved) &&
-				h.filtersModeService.Map(formattedText, forwardRule) == entity.FiltersCheck {
-
-				_, ok := checkFns[forwardRule.Check]
-				if !ok {
-					checkFns[forwardRule.Check] = func() {
-						const isSendCopy = false // обязательно надо форвардить, иначе не видно текущего сообщения
-						h.forwarderService.ForwardMessages([]*client.Message{src}, src.ChatId, forwardRule.Check, isSendCopy, forwardRuleId)
-					}
-				}
-				continue
-			}
-			// hasFiltersCheck := false
-			// testChatId := dstChatId
-			// for _, forward := range configData.Forwards {
-			// 	if src.ChatId == forward.From && (forward.SendCopy || src.CanBeForwarded) {
-			// 		for _, dstChatId := range forward.To {
-			// 			if testChatId == dstChatId {
-			// 				if checkFilters(formattedText, forward) == FiltersCheck {
-			// 					hasFiltersCheck = true
-			// 					_, ok := checkFns[forward.Check]
-			// 					if !ok {
-			// 						checkFns[forward.Check] = func() {
-			// 							const isSendCopy = false // обязательно надо форвардить, иначе не видно текущего сообщения
-			// 							forwardNewMessages(tdlibClient, []*client.Message{src}, src.ChatId, forward.Check, isSendCopy)
-			// 						}
-			// 					}
-			// 				}
-			// 			}
-			// 		}
-			// 	}
-			// }
-			// if hasFiltersCheck {
-			// 	continue
-			// }
-			withSources := true
-			if err := h.transformService.Transform(formattedText, withSources, src, dstChatId); err != nil {
-				h.log.Error("Transform", "err", err)
-			}
-			tmpChatMessageId := fmt.Sprintf("%d:%d", dstChatId, tmpMessageId)
-			newMessageId := newMessageIds[tmpChatMessageId]
-			result = append(result, fmt.Sprintf("toChatMessageId: %s, newMessageId: %d", toChatMessageId, newMessageId))
-			switch src.Content.(type) {
-			case
-				*client.MessageText,
-				*client.MessageAnimation,
-				*client.MessageDocument,
-				*client.MessageAudio,
-				*client.MessageVideo,
-				*client.MessagePhoto:
-				content := h.messageService.GetInputMessageContent(src, formattedText)
-				dst, err := h.telegramRepo.GetClient().EditMessageText(&client.EditMessageTextRequest{
-					ChatId:              dstChatId,
-					MessageId:           newMessageId,
-					InputMessageContent: content,
-					// ReplyMarkup: func() client.ReplyMarkup {
-					// 	if src.Content.(type).MessageContentType() == client.TypeMessageText {
-					// 		return src.ReplyMarkup // это не надо, юзер-бот игнорит изменение
-					// 	}
-					// 	return nil
-					// }(),
-				})
-				if err != nil {
-					log.Print("EditMessageText > ", err)
-				}
-				log.Printf("EditMessageText > dst: %#v", dst)
-			case *client.MessageVoiceNote:
-				dst, err := h.telegramRepo.GetClient().EditMessageCaption(&client.EditMessageCaptionRequest{
-					ChatId:    dstChatId,
-					MessageId: newMessageId,
-					Caption:   formattedText,
-				})
-				if err != nil {
-					log.Print("EditMessageCaption > ", err)
-				}
-				log.Printf("EditMessageCaption > dst: %#v", dst)
-			default:
-				continue
-			}
-			// TODO: isAnswer
-			if hasReplyMarkupData {
-				h.storageService.SetAnswerMessageId(dstChatId, tmpMessageId, fromChatMessageId)
-			} else {
-				h.storageService.DeleteAnswerMessageId(dstChatId, tmpMessageId)
-			}
-		}
-		for check, fn := range checkFns {
-			if fn == nil {
-				log.Printf("check: %d is nil", check)
-				continue
-			}
-			log.Printf("check: %d is fn()", check)
-			fn()
+		if hasReplyMarkupData {
+			h.storageService.SetAnswerMessageId(dstChatId, tmpMessageId, fromChatMessageId)
+		} else {
+			h.storageService.DeleteAnswerMessageId(dstChatId, tmpMessageId)
 		}
 	}
-	h.queueRepo.Add(fn)
+
+	for _, fn := range checkFns {
+		fn()
+	}
+
+	return result, nil
 }
