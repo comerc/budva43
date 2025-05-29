1ffa8cd3861117b77e98c51e0cd6b204bf4f24bc
comerc <andrew.kachanov@gmail.com>
2025-05-29 19:52:49 +0300

    Fix argsPtr

--- util/log.go
+++ util/log.go
@@ -23,21 +23,23 @@ func (l *Logger) logOrError(level slog.Level, message string, errPtr *error, arg
 		level = slog.LevelError
 		args = append(args, "err", err)
 	}
-	args = append(args, (*argsPtr)...)
+	if argsPtr != nil {
+		args = append(args, (*argsPtr)...)
+	}
 	for i, call := range GetCallStack(3, 0) {
 		args = append(args, fmt.Sprintf("stack[%d]", i), call)
 	}
 	l.Log(context.Background(), level, message, args...)
 }
 
-func (l *Logger) DebugOrError(message string, errPtr *error, argsPtr *[]any) {
-	l.logOrError(slog.LevelDebug, message, errPtr, argsPtr)
-}
+// func (l *Logger) DebugOrError(message string, errPtr *error, argsPtr *[]any) {
+// 	l.logOrError(slog.LevelDebug, message, errPtr, argsPtr)
+// }
 
 func (l *Logger) InfoOrError(message string, errPtr *error, argsPtr *[]any) {
 	l.logOrError(slog.LevelInfo, message, errPtr, argsPtr)
 }
 
-func (l *Logger) WarnOrError(message string, errPtr *error, argsPtr *[]any) {
-	l.logOrError(slog.LevelWarn, message, errPtr, argsPtr)
-}
+// func (l *Logger) WarnOrError(message string, errPtr *error, argsPtr *[]any) {
+// 	l.logOrError(slog.LevelWarn, message, errPtr, argsPtr)
+// }
