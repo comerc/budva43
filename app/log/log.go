b2fffa2199acd59b501164c27f62beaf853bf7f2
comerc <andrew.kachanov@gmail.com>
2025-06-04 13:47:24 +0300

    #80 Extract error.go

--- app/log/log.go
+++ app/log/log.go
@@ -21,30 +21,6 @@ func NewLogger(moduleName string) *Logger {
 	}
 }
 
-func (l *Logger) logOrError(level slog.Level, message string, errPtr *error, args ...any) {
-	var err error
-	if errPtr != nil && *errPtr != nil {
-		err = *errPtr
-		message = err.Error()
-		level = slog.LevelError
-	}
-	if err != nil {
-		var stack []*CallInfo
-		var customError *CustomError
-		if errors.As(err, &customError) {
-			args = append(args, customError.Args...)
-			stack = customError.Stack
-		}
-		if stack == nil {
-			stack = GetCallStack(3, 0)
-		}
-		for i, item := range stack {
-			args = append(args, fmt.Sprintf("stack[%d]", i), item)
-		}
-	}
-	l.log.Log(context.Background(), level, message, args...)
-}
-
 func (l *Logger) DebugOrError(message string, errPtr *error, args ...any) {
 	l.logOrError(slog.LevelDebug, message, errPtr, args...)
 }
@@ -57,35 +33,28 @@ func (l *Logger) WarnOrError(message string, errPtr *error, args ...any) {
 	l.logOrError(slog.LevelWarn, message, errPtr, args...)
 }
 
-func WrapError(err error, args ...any) error {
-	if err == nil {
-		err = errors.New("err is nil")
-	} else {
+func (l *Logger) logOrError(level slog.Level, message string, errPtr *error, args ...any) {
+	var err error
+	if errPtr != nil && *errPtr != nil {
+		err = *errPtr
+		message = err.Error()
+		level = slog.LevelError
+	}
+	if err != nil {
+		var stack []*CallInfo
 		var customError *CustomError
 		if errors.As(err, &customError) {
-			customError.Args = append(customError.Args, args...)
-			return customError
+			args = append(customError.Args, args...)
+			stack = customError.Stack
+		}
+		if stack == nil {
+			stack = GetCallStack(3, 0)
+		}
+		for i, item := range stack {
+			args = append(args, fmt.Sprintf("stack[%d]", i), item)
 		}
 	}
-	return &CustomError{
-		error: err,
-		Args:  args,
-		Stack: GetCallStack(2, 0),
-	}
-}
-
-func NewError(text string, args ...any) error {
-	return &CustomError{
-		error: errors.New(text),
-		Args:  args,
-		Stack: GetCallStack(2, 0),
-	}
-}
-
-type CustomError struct {
-	error
-	Args  []any
-	Stack []*CallInfo
+	l.log.Log(context.Background(), level, message, args...)
 }
 
 func setupLogger() {
--- app/log/log_test.go
+++ app/log/log_test.go
@@ -28,14 +28,13 @@ func (s *SomeObject) NestedMethod() {
 	var err error
 	defer func() {
 		args := []any{
-			"arg1", "val1",
 			"arg2", "val2",
 		}
 		s.log.DebugOrError("message", &err, args...)
 	}()
 
 	err = NewError("error", "arg0", "val0")
-	err = WrapError(err, "arg3", "val3")
+	err = WrapError(err, "arg1", "val1")
 }
 
 func TestSomeMethod(t *testing.T) {
@@ -53,8 +52,7 @@ func TestSomeMethod(t *testing.T) {
 	assert.Equal(t, "val0", spylog.GetAttrValue(record0, "arg0"))
 	assert.Equal(t, "val1", spylog.GetAttrValue(record0, "arg1"))
 	assert.Equal(t, "val2", spylog.GetAttrValue(record0, "arg2"))
-	assert.Equal(t, "val3", spylog.GetAttrValue(record0, "arg3"))
-	assert.Equal(t, "app/log/log_test.go:37 log.(*SomeObject).NestedMethod",
+	assert.Equal(t, "app/log/log_test.go:36 log.(*SomeObject).NestedMethod",
 		spylog.GetAttrValue(record0, "stack[0]"))
 }
 
@@ -71,54 +69,14 @@ func TestSimpleError(t *testing.T) {
 	})
 
 	err := errors.New("simple error")
-	o.log.InfoOrError("message", &err)
+	o.log.InfoOrError("message", &err, "arg", "val")
 
 	require.True(t, len(spylogHandler.Records) == 1)
 	record0 := spylogHandler.Records[0]
 
 	assert.Equal(t, slog.LevelError, record0.Level)
 	assert.Equal(t, "simple error", record0.Message)
-	assert.Equal(t, "app/log/log_test.go:74 log.TestSimpleError",
+	assert.Equal(t, "val", spylog.GetAttrValue(record0, "arg"))
+	assert.Equal(t, "app/log/log_test.go:72 log.TestSimpleError",
 		spylog.GetAttrValue(record0, "stack[0]"))
-}
-
-func TestWrapError(t *testing.T) {
-	tests := []struct {
-		name          string
-		err           error
-		args          []any
-		expectedError string
-		expectedArgs  []any
-	}{
-		{
-			name:          "nil error",
-			err:           nil,
-			args:          []any{"arg1", "val1"},
-			expectedError: "err is nil",
-			expectedArgs:  []any{"arg1", "val1"},
-		},
-		{
-			name:          "not CustomError",
-			err:           errors.New("error"),
-			args:          []any{"arg1", "val1"},
-			expectedError: "error",
-			expectedArgs:  []any{"arg1", "val1"},
-		},
-		{
-			name:          "CustomError",
-			err:           NewError("error", "arg0", "val0"),
-			args:          []any{"arg1", "val1"},
-			expectedError: "error",
-			expectedArgs:  []any{"arg0", "val0", "arg1", "val1"},
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			err, ok := WrapError(test.err, test.args...).(*CustomError)
-			assert.True(t, ok)
-			assert.Equal(t, test.expectedError, err.Error())
-			assert.Equal(t, test.expectedArgs, err.Args)
-			assert.NotNil(t, err.Stack)
-		})
-	}
 }
