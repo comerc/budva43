62686a740ea67b84c3b7d4087f4bea06af8804a0
comerc <andrew.kachanov@gmail.com>
2025-06-19 10:23:08 +0300

    #22 Extract util.GetConfigOptions()

---
+++ app/util/config_options.go
@@ -0,0 +1,42 @@
+package util
+
+import (
+	"reflect"
+
+	"github.com/mitchellh/mapstructure"
+	"github.com/samber/lo"
+	"github.com/spf13/viper"
+)
+
+// Настраиваем декодирование
+func GetConfigOptions() viper.DecoderConfigOption {
+	return viper.DecodeHook(
+		mapstructure.ComposeDecodeHookFunc(
+			mapstructure.StringToTimeDurationHookFunc(),
+			mapstructure.StringToSliceHookFunc(","),
+			kebabCaseKeyHookFunc(),
+		),
+	)
+}
+
+func kebabCaseKeyHookFunc() mapstructure.DecodeHookFunc {
+	return func(from reflect.Type, _ reflect.Type, data any) (any, error) {
+		if from.Kind() != reflect.Map {
+			return data, nil
+		}
+
+		m, ok := data.(map[string]any)
+		if !ok {
+			return data, nil
+		}
+
+		// Создаем новую карту с преобразованными ключами
+		out := make(map[string]any)
+		for k, v := range m {
+			// Преобразуем ключ из kebab-case в PascalCase
+			pascalKey := lo.PascalCase(k)
+			out[pascalKey] = v
+		}
+		return out, nil
+	}
+}
