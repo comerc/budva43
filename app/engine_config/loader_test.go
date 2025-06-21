84a57ef05b65a6ac59164d0d58b03393dadd3fbc
comerc <andrew.kachanov@gmail.com>
2025-06-21 08:20:00 +0300

    #84 Add engine_config.TestCheck()

---
+++ app/engine_config/loader_test.go
@@ -0,0 +1,28 @@
+package engine_config
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/comerc/budva43/app/entity"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCheck(t *testing.T) {
+	t.Parallel()
+
+	err := check(&entity.EngineConfig{})
+	var emptyConfigData *ErrEmptyConfigData
+	require.True(t, errors.As(err, &emptyConfigData))
+
+	assert.Equal(t, err.Error(), "отсутствуют данные")
+	assert.Equal(t, emptyConfigData.Args, []any{
+		"path[0]",
+		"config.Engine.UniqueSources",
+		"path[1]",
+		"config.Engine.UniqueDestinations",
+		"path[2]",
+		"config.Engine.OrderedForwardRules",
+	})
+}
