5178e9641bdaa6f8abe837f3344012ddc7f478ee
comerc <andrew.kachanov@gmail.com>
2025-06-25 13:30:09 +0300

    #84 Fix copy

--- app/log/error.go
+++ app/log/error.go
@@ -31,10 +31,8 @@ func WrapError(err error, args ...any) error {
 		newArgs := make([]any, 0, len(existing.Args)+len(args))
 		newArgs = append(newArgs, existing.Args...)
 		newArgs = append(newArgs, args...)
-		// newStack := make([]*CallInfo, 0, len(existing.Stack))
-		// newStack = append(newStack, existing.Stack...)
-		var newStack []*CallInfo
-		copy(newStack, existing.Stack)
+		newStack := make([]*CallInfo, 0, len(existing.Stack))
+		newStack = append(newStack, existing.Stack...)
 		return &CustomError{
 			error: existing.Unwrap(),
 			Args:  newArgs,
